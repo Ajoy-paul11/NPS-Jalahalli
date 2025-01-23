95eca32a77006a7a932e4807302585f7fdd207e2
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-01-23 18:47:22 +0530

    Add HeroSection and Footer components; update index.html for Google Tag Manager integration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { Button } from "./components/ui/button"
-import FormAdmission from "./components/Form/Form"
+import { Button } from "./components/ui/button";
+import FormAdmission from "./components/Form/Form";
+import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-    <div className="flex items-center justify-center bg-[#181818] h-screen">
-      <div className=" shadow-lg shadow-[#7b7878] py-6 px-8 rounded-lg">
-      <FormAdmission />
+      <div className=" bg-[#f4f5ff] min-h-screen overflow-hidden">
+        <HeroSection />
+        {/* <div className=" shadow-lg shadow-[#7b7878] py-6 px-8 rounded-lg w-1/4">
+          
+        </div> */}
+        <About />
+        <Footer />
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className=" p-8 mt-6 flex flex-col lg:flex-row justify-evenly items-center lg:items-end  gap-8 bg-blue-100">
+      <div className=" max-w-lg text-lg">
+       <span className=" text-blue">&copy; Copyrights </span> {new Date().getFullYear()}{" "}
+        <span>National Public School, Jalahalli. <br className=" hidden lg:block"/> All Rights Reserved.</span>
+      </div>
+      <div className=" max-w-lg">
+        <h4 className=" text-2xl font-semibold text-center text-blue mb-4">Contact Us</h4>
+        <p className=" flex items-center gap-6 text-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="80"
+            height="80"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-map-pinned text-blue"
+          >
+            <path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0" />
+            <circle cx="12" cy="8" r="2" />
+            <path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712" />
+          </svg>
+          Gangamma Gudi Police Station road, Behind Shriram Sameeksha
+          apartments, Shriniket Layout, Singapura, Jalahalli East, Bangalore -
+          560015.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
