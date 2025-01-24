8fed563276ff0ad15eefd99d0d6a8351bc364e02
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-01-24 12:38:18 +0530

    Add .env files to .gitignore; integrate react-toastify for form submission feedback; refactor form to handle terms and conditions with improved error handling and loading state.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,10 +17,10 @@ function Footer() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-map-pinned text-blue"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-map-pinned text-blue"
           >
             <path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0" />
             <circle cx="12" cy="8" r="2" />
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -22,24 +22,49 @@ import {
 } from "@/components/ui/select";
 import { useNavigate } from "react-router-dom";
 import { Checkbox } from "@/components/ui/checkbox";
+import { toast } from "react-toastify";
 
 function FormAdmission() {
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
   const form = useForm({
     defaultValues: {
       name: "",
       email: "",
       phone: "",
       class: "",
-      terms: false,
+      terms_and_conditions: false,
     },
   });
 
   const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    form.reset();
-    navigate("/thankyou");
+  const onSubmit = async (data) => {
+    try {
+      setIsSubmitting(true);
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      
+      if (result.success) {
+        console.log(data);
+        toast.success("Message sent Successfully");
+        form.reset();
+        navigate("/thankyou");
+      } else {
+        toast.error(result.message || "Failed to send message");
+      }
+    } catch (error) {
+      toast.error(error.message || "Internal Server Error, Please try again!");
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
@@ -52,6 +77,17 @@ function FormAdmission() {
           onSubmit={form.handleSubmit(onSubmit)}
           className="  px-6 pb-6 rounded-lg  m-0"
         >
+          <input
+            type="hidden"
+            value={import.meta.env.VITE_FORM_API_KEY}
+            {...form.register("access_key")}
+          />
+          <input type="hidden" {...form.register("subject")} />
+          <input
+            type="hidden"
+            value="NPS Jalahalli Website"
+            {...form.register("from_name")}
+          />
           <FormField
             name="name"
             control={form.control}
@@ -67,7 +103,7 @@ function FormAdmission() {
                     placeholder="Student Name..."
                     required
                     {...field}
-                    className=" text-slate-200"
+                    className=" text-gray-200"
                   />
                 </div>
               </FormItem>
@@ -88,7 +124,7 @@ function FormAdmission() {
                     placeholder="Email Address..."
                     required
                     {...field}
-                    className=" text-slate-200"
+                    className=" text-gray-200"
                   />
                 </div>
               </FormItem>
@@ -109,7 +145,7 @@ function FormAdmission() {
                     placeholder="Phone Number..."
                     required
                     {...field}
-                    className=" text-slate-200"
+                    className=" text-gray-200"
                   />
                 </div>
               </FormItem>
@@ -156,21 +192,21 @@ function FormAdmission() {
             )}
           />
           <FormField
-            name="terms"
+            name="terms_and_conditions"
             control={form.control}
             rules={{ required: "Please accept the terms" }}
             render={({ field }) => (
               <FormItem className="space-y-0 my-3">
                 <div className="items-top flex space-x-2">
                   <Checkbox
-                    id="terms"
+                    id="terms_and_conditions"
                     checked={field.value}
                     onCheckedChange={field.onChange}
                     className=" border-gray-200 data-[state=checked]:bg-orange-500 data-[state=checked]:border-white"
                   />
                   <div className="grid gap-1.5 leading-none">
                     <label
-                      htmlFor="terms"
+                      htmlFor="terms_and_conditions"
                       className="text-sm text-gray-200 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                     >
                       I agree to receive information regarding my submitted
@@ -183,9 +219,9 @@ function FormAdmission() {
           />
           <Button
             type="submit"
-            className=" bg-orange-500 text-gray-200 hover:bg-orange-300 hover:text-gray-800 w-full mt-3"
+            className={`bg-orange-500 text-gray-200 hover:bg-orange-300 hover:text-gray-800 w-full mt-3 ${isSubmitting ? "cursor-not-allowed" : ""}`}
           >
-            Submit
+            {isSubmitting ? "Sending" : "Send Message"}
           </Button>
         </form>
       </Form>
