f59ad3164c1a587c58f0a04d91c90d113bac0653
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-02-01 18:08:44 +0530

    Enhance School component; emphasize school name with strong tags for improved visibility and branding.

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -9,7 +9,7 @@ function School() {
           National Public School Jalahalli -Best CBSE School in North Bangalore
         </h1>
         <p className=" text-lg text-black my-2 indent-8">
-          NPS, Jalahalli has a sprawling 8 acres campus with state-of-the-art
+          <strong>NPS Jalahalli</strong> has a sprawling 8 acres campus with state-of-the-art
           infrastructure comprising of facilities like fully equipped Labs,
           Audio Visual Room, art centre, Music room, Conference Room,
           Multipurpose Hall, Health centre, Counselling Centre, Athletic and
@@ -17,7 +17,7 @@ function School() {
           Cricket and Football field and various indoor sports.
         </p>
         <p className=" text-lg text-black my-4 indent-8">
-          National Public School, Jalahalli is a progressive child-centric
+          <strong> National Public School Jalahalli </strong> is a progressive child-centric
           School, driven by its core values, set in a warm and nurturing
           Environment, emphasizing personalized attention, upholding academic
           excellence and the holistic development of each child. We attribute it
